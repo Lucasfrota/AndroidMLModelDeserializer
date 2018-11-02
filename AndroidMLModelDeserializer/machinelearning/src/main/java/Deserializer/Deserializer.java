372eed7aa5c771f6034d015a7b9491ed1ad1281c
Lucas Frota <lucas.frota@oceanbrasil.com>
2018-11-01 21:16:54 -0400

    getMLModel update

--- AndroidMLModelDeserializer/machinelearning/src/main/java/Deserializer/Deserializer.java
+++ AndroidMLModelDeserializer/machinelearning/src/main/java/Deserializer/Deserializer.java
@@ -1,12 +1,16 @@
 package Deserializer;
 
 import android.content.Context;
+import android.nfc.Tag;
+import android.util.Log;
 
 import java.io.ObjectInputStream;
 
 import machinelearning.ElasticClassifier;
 
 public class Deserializer {
+
+    final static String TAG = "DeserializerTag";
 
     public static ElasticClassifier getMLModel(Context context, String fileName) {
 
@@ -15,9 +19,9 @@ public class Deserializer {
             ElasticClassifier localElasticClassifier = (ElasticClassifier)localObjectInputStream.readObject();
             localObjectInputStream.close();
             return localElasticClassifier;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
+            Log.d(TAG, e.toString());
         }
         return null;
     }
